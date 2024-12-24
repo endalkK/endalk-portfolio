d5c6a4f6927e77835caca71598362506c4ba9eef
endalkK <endalkkelemu@gmail.com>
2024-12-24 17:27:08 -0500

    updated with neat and more stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,7 +128,7 @@ const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
-    delay: 400, //reset true // Animations repeat
+    delay: 400,
 })
 sr.reveal('.home__perfil, .about__image, .contact__mail', {origin:'right'})
 sr.reveal('.home__name, home__info, .about__container section__title-1, .about__info, .contact__social, .contact__data', {origin: 'left'})
